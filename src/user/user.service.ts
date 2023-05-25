5673f29c54a6e4f966da495668d22053d0761f35
jeandson <jeandsontb@gmail.com>
2023-05-24 21:46:16 -0300

    adjust user

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,30 +1,29 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/createUser.dto';
-import { User } from './interface/user.interface';
+import { UserEntity } from './entity/user.entity';
 
 import { hash } from 'bcrypt';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UserService {
-  private users: User[] = [];
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>,
+  ) {}
 
-  async createUser(createUserDto: CreateUserDto): Promise<User> {
+  async createUser(createUserDto: CreateUserDto): Promise<UserEntity> {
     const saltRoundCrypt = 10;
-
     const newHash = await hash(createUserDto.password, saltRoundCrypt);
 
-    const user = {
+    return this.userRepository.save({
       ...createUserDto,
-      id: this.users.length + 1,
       password: newHash,
-    };
-
-    this.users.push(user);
-
-    return user;
+    });
   }
 
-  async getAllUser(): Promise<User[]> {
-    return this.users;
+  async getAllUser(): Promise<UserEntity[]> {
+    return this.userRepository.find();
   }
 }
